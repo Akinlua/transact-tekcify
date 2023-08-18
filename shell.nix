98f3cc7b2ba07aecb89b59539641334bfd2f03fa
Tesals <85995048+Akinlua@users.noreply.github.com>
2023-08-18 18:54:43 +0100

    Update shell.nix

--- shell.nix
+++ shell.nix
@@ -2,12 +2,12 @@
 
 pkgs.mkShell {
   buildInputs = [
-    pkgs.libuuid
+    pkgs.libuuid1
     # ... other dependencies
   ];
 
   # Set the library path (LD_LIBRARY_PATH) to include the libuuid library
   shellHook = ''
-    export LD_LIBRARY_PATH=${pkgs.libuuid}/lib:$LD_LIBRARY_PATH
+    export LD_LIBRARY_PATH=${pkgs.libuuid1}/lib:$LD_LIBRARY_PATH
   '';
 }
